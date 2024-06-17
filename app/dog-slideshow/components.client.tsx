db22e41a363a641ff9293dafcce8cf2a4efff431
Khinshan Khan <shan@uptogood.dev>
2024-06-17 18:18:08 -0400

    part 1 matches mockup

--- app/dog-slideshow/components.client.tsx
+++ app/dog-slideshow/components.client.tsx
@@ -1,16 +1,51 @@
 "use client"
 
 import React, { useEffect, useState } from "react"
+import { getDogs } from "./utils"
 
 export function Foo() {
-  const [message, setMessage] = useState("loading")
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setMessage("Hello from foo")
-    }, 1000)
+  const [loading, setLoading] = useState(true)
+  const [dogs, setDogs] = useState<{ title: string; url: string }[]>([])
+  const [carouselIndex, setCarouselIndex] = useState(0)
 
-    return () => clearTimeout(timeout)
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDogs()
+      setDogs(data)
+      setLoading(false)
+    }
+
+    fetchData()
   }, [])
 
-  return <div>{message}</div>
+  if (loading) return <div>Loading...</div>
+
+  const currentDog = dogs[carouselIndex]
+  return (
+    <div className="m-8 flex w-full flex-col gap-4">
+      <img
+        alt={currentDog.title}
+        src={currentDog.url}
+        className="mx-auto max-h-[500px] max-w-[500px]"
+      />
+
+      <div className="flex flex-row justify-center gap-2">
+        <button
+          className="size-[50px] bg-purple-400 hover:bg-purple-700"
+          onClick={() => setCarouselIndex((carouselIndex - 1 + dogs.length) % dogs.length)}
+        >
+          {"<"}
+        </button>
+
+        <span className="w-[500px] text-balance text-center text-2xl">{currentDog.title}</span>
+
+        <button
+          className="size-[50px] items-center justify-center bg-purple-400 hover:bg-purple-700"
+          onClick={() => setCarouselIndex((carouselIndex + 1) % dogs.length)}
+        >
+          {">"}
+        </button>
+      </div>
+    </div>
+  )
 }
--- app/dog-slideshow/page.tsx
+++ app/dog-slideshow/page.tsx
@@ -4,8 +4,6 @@ import { Foo } from "./components.client"
 export default function Page() {
   return (
     <main className="grow">
-      <p>Hello there</p>
-      <p className="text-purple-700">This is purple</p>
       <Foo />
     </main>
   )
